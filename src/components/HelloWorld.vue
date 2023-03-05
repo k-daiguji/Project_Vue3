d0577a0d134692257707d31531b7ae615c5c573c
k-daiguji <hikatteru20@gmail.com>
2023-03-05 17:33:39 +0900

    Vuetify3.0導入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <v-btn>test</v-btn>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,9 +20,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
